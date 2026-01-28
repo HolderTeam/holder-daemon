4292315c6e85dd607f5cb8a04f9b5c394d79355d
Zeth <theology@gmail.com>
2026-01-28 20:32:06 +0000

    Added test.

--- tests/HttpAuth_test.cpp
+++ tests/HttpAuth_test.cpp
@@ -91,6 +91,39 @@ TEST_CASE("HTTP endpoints require auth token", "[http]") {
   const auto lower_bearer = raw_with_auth("bearer " + token);
   REQUIRE(lower_bearer == boost::beast::http::status::unauthorized);
 
+  const auto spaced_bearer = raw_with_auth("Bearer    " + token);
+  REQUIRE(spaced_bearer == boost::beast::http::status::unauthorized);
+
+  auto raw_with_multi_auth = [&](const std::string& first, const std::string& second) {
+    namespace http = boost::beast::http;
+    using tcp = boost::asio::ip::tcp;
+
+    boost::asio::io_context ioc;
+    tcp::resolver resolver(ioc);
+    auto endpoints = resolver.resolve(bound.bind, std::to_string(bound.port));
+
+    tcp::socket socket(ioc);
+    boost::asio::connect(socket, endpoints);
+
+    http::request<http::string_body> req{http::verb::get, "/health", 11};
+    req.set(http::field::host, bound.bind);
+    req.set(http::field::user_agent, "holder-tests");
+    req.insert(http::field::authorization, first);
+    req.insert(http::field::authorization, second);
+
+    http::write(socket, req);
+
+    boost::beast::flat_buffer buffer;
+    http::response<http::string_body> res;
+    http::read(socket, buffer, res);
+    socket.shutdown(tcp::socket::shutdown_both);
+
+    return res.result();
+  };
+
+  const auto multi_auth = raw_with_multi_auth("Token nope", "Bearer " + token);
+  REQUIRE(multi_auth == boost::beast::http::status::unauthorized);
+
   std::raise(SIGTERM);
   server_thread.join();
 }
